879df3b34590bb57407a796d06fa50d2906bb383
임성현 <shlim0287@gmail.com>
2024-01-18 09:45:02 +0900

    refactor : 선언된 변수들 전역변수로 변경.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 // script.js
 
-document.addEventListener("DOMContentLoaded", function () {
-  let emailInput = document.getElementById("email");
-  let emailIcon = document.getElementById("emailIcon");
+let emailIcon = document.getElementById("emailIcon");
+let emailInput = document.getElementById("email");
 
+let openModalBtn = document.getElementById("openModalBtn");
+let closeModalBtn = document.getElementById("closeModalBtn");
+let modal = document.getElementById("subs_modal");
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
   if (emailInput && emailIcon) {
     emailInput.addEventListener("focus", function () {
       emailIcon.style.display = "none";
@@ -16,36 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  emailInput.addEventListener("input",function (){
-    if(!emailInput.checkValidity()){
-      emailInput.placeholder="Invalid email address";
-    }
-  });
+  // emailInput.addEventListener("input",function (){
+  //   if(!emailInput.checkValidity()){
+  //     // emailInput.placeholder="Invalid email address";
+  //   }
+  // });
 });
 
 
 
 
-// 모달 열기 버튼
-let openModalBtn = document.getElementById("openModalBtn");
-
-// // 모달 닫기 버튼
-let closeModalBtn = document.getElementById("closeModalBtn");
-
-// 모달 요소
-let modal = document.getElementById("subs_modal");
-
-// 열기 버튼을 클릭하면 모달을 표시
 openModalBtn.onclick = function () {
-  // 유효하지 않은 이메일이면 모달을 띄우지 않음
-  if (!emailInput.checkValidity()) {
-    emailInput.placeholder="Invalid email address";
-  } else {
+  if (emailInput.checkValidity())
     modal.style.display = "block";
-  }
 };
 
-// // 닫기 버튼을 클릭하거나 모달 외부를 클릭하면 모달을 숨김
 closeModalBtn.onclick = function() {
   modal.style.display = "none";
 }
