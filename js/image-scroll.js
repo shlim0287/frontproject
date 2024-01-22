35d6add132d6d67dea805b7c60b941b5ddbde950
임성현 <shlim0287@gmail.com>
2024-01-22 13:47:32 +0900

    feat : 무한스크롤 기능 구현.(정지기능 추가 예정)

--- js/image-scroll.js
+++ js/image-scroll.js
@@ -1,6 +1,8 @@
 const imageList = document.querySelector(".img-grid");
 let pageToFetch = 1;
 
+let fetching = false;
+
 async function fetchImages(pageNum) {
   try {
     const response = await fetch('https://picsum.photos/v2/list?page=' + pageNum + '&limit=6');
@@ -15,6 +17,8 @@ async function fetchImages(pageNum) {
 
   } catch (error) {
     console.error('데이터를 가져오는데 문제가 발생했습니다 :', error);
+  } finally {
+    fetching = false; // 무한 스크롤 완료 후 플래그를 false로 설정
   }
 }
 
@@ -26,24 +30,25 @@ function makeImageList(datas) {
   });
 }
 
-document.getElementById('show_more_btn').addEventListener('click',()=>{
-  fetchImages(pageToFetch++);
-});
-window.addEventListener('scroll',()=>{
-
-});
-
+// 최초 로딩 시 이미지 가져오기
 fetchImages(pageToFetch);
 
+document.getElementById('show_more_btn').addEventListener('click', () => {
+  if (!fetching) {
+    fetching = true; // 쇼 모어 버튼 클릭 시 fetching 플래그를 true로 설정
+    fetchImages(pageToFetch);
+    window.addEventListener("scroll", handleScroll);
+  }
+});
 
-
-//
-//
-// window.addEventListener("scroll", () => {
-//   if (window.innerHeight + document.documentElement.scrollTop + 10 >=
-//     document.documentElement.offsetHeight) {
-//     fetchImages(pageToFetch++);
-//   }
-// });
-
+// 스크롤 이벤트 처리 함수
+function handleScroll() {
+  if (imageList.scrollTop + imageList.clientHeight + 10 >= imageList.scrollHeight) {
+    if (!fetching) {
+      fetching = true; // 스크롤 이벤트 발생 시 fetching 플래그를 true로 설정
+      fetchImages(pageToFetch++);
+    }
+  }
+}
+imageList.addEventListener("scroll", handleScroll);
 
